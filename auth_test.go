48cf42cff014cd5ee97e6f91a9ed6ab987c9454c
hakem26 <hamed26km@gmail.com>
2025-12-06 23:38:36 +0330

    add to first day

--- auth_test.go
+++ auth_test.go
@@ -1,6 +1,28 @@
 package twitterapp
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegisterInput_Sanitize(t *testing.T)  {
+	input := RegisterInput{
+		Username: " bob ",
+		Email: " Bob@gmail.Com ",
+		Password: "Pas123",
+		ConfirmPassword: "Pas123",
+	}
+
+	want := RegisterInput{
+		Username: "bob",
+		Email: "bob@gmail.com",
+		Password: "Pas123",
+		ConfirmPassword: "Pas123",
+	}
+
+	input.Sanitize()
+	require.Equal(t, want, input)
+}
 
 func TestRegisterInput_Validate(t *testing.T) {
 	testCases := []struct{
@@ -18,5 +40,55 @@ func TestRegisterInput_Validate(t *testing.T) {
 			},
 			err: nil,
 		},
+		{
+			name: "invalid email",
+			input: RegisterInput{
+				Username: "hkm",
+				Email: "hkmmail.co",
+				Password: "pas123",
+				ConfirmPassword: "pas123",
+			},
+			err: ErrValidation,
+		},
+		{
+			name: "too short username",
+			input: RegisterInput{
+				Username: "h",
+				Email: "hkm@mail.co",
+				Password: "pas123",
+				ConfirmPassword: "pas123",
+			},
+			err: ErrValidation,
+		},
+		{
+			name: "too short password",
+			input: RegisterInput{
+				Username: "hkm",
+				Email: "hkm@mail.co",
+				Password: "pas1",
+				ConfirmPassword: "pas1",
+			},
+			err: ErrValidation,
+		},
+		{
+			name: "confirm password did not match",
+			input: RegisterInput{
+				Username: "hkm",
+				Email: "hkm@mail.co",
+				Password: "pas123",
+				ConfirmPassword: "pas456",
+			},
+			err: ErrValidation,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err:= tc.input.Validate()
+			if err != nil {
+				require.ErrorIs(t, err, tc.err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
 	}
 }
